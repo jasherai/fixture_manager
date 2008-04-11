712802bd75be1aed55dbd6c8f5125450448144fe
Andrew Bloom <abloom@andrew-blooms-imac.local>
2008-04-11 09:50:35 -0500

    locking the plugin out of the test environment. causes all sorts of havoc there

--- init.rb
+++ init.rb
@@ -1,5 +1,6 @@
-# Include hook code here
-base_path = File.dirname(__FILE__) + "/lib/"
-require base_path + "fixture_manager" 
-require base_path + "active_record/fixture_management"
-require base_path + "active_record/many_to_many_fixtures"
+unless RAILS_ENV == 'test'
+  base_path = File.dirname(__FILE__) + "/lib/"
+  require base_path + "fixture_manager" 
+  require base_path + "active_record/fixture_management"
+  require base_path + "active_record/many_to_many_fixtures"
+end
